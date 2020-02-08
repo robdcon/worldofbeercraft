93f5559cd6269c97a05c415883626ef9c9c17352
robdcon <robdcon@gmail.com>
2020-02-08 10:55:45 +0000

    adds test for pipe

--- src/app/strength/strength.pipe.spec.ts
+++ src/app/strength/strength.pipe.spec.ts
@@ -1,5 +1,12 @@
-describe("Strength pipe", () => {
-    it("should display weak if strength is less than 4", () => {
-        
+import { StrengthPipe } from './strength.pipe';
+
+describe('Strength pipe', () => {
+    it('should display weak if strength is less than 4', () => {
+
+        const pipe = new StrengthPipe();
+
+        const val = pipe.transform(5);
+
+        expect(val).toEqual('5 (weak)');
     })
 })
