b7fd6058beac81425d13cba93c7c83e0f331f703
robdcon <robdcon@gmail.com>
2020-02-10 23:03:38 +0000

    adds form onchange functions

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
+  value: string;
   constructor() { }
+
+  setLevel(level) {
+    this.value = level;
+    console.log(this.value);
+  }
 
   ngOnInit(): void {
   }
